a9df3b05e56caa47e68a4712d3ef4000af555f77
Junior <juniorod99@gmail.com>
2025-02-05 23:33:42 -0300

    Iniciando projeto

---
+++ dados.php
@@ -0,0 +1,13 @@
+<?php
+$livros = [
+    ['id' => '1', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '2', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '3', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '4', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '5', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '6', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '7', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '8', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+    ['id' => '9', 'titulo' => 'Senhor do Aneis', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Lorem impsu asodao asoidjaoidjaiod asidjasiodasiodasiodj asodja siodjasiod'],
+
+];
