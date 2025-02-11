d7522f6f604be58985df2f07d0e82aad9b0bcdb8
Junior <juniorod99@gmail.com>
2025-02-11 18:55:27 -0300

    Adicionando configuração com outros DBs

--- config.php
+++ config.php
@@ -4,6 +4,14 @@ return [
     'database' => [
         'driver' => 'sqlite',
         'database' => 'database.sqlite',
+
+        // 'driver' => 'mysql',
+        // 'host' => 'localhost',
+        // 'port' => '3306',
+        // 'dbname' => 'bookwise',
+        // 'user' => 'root',
+        // 'password' => 'root',
+        // 'charset' => 'utf8mb4',
     ],
 
 ];
--- database.php
+++ database.php
@@ -6,8 +6,21 @@ class DB
 
     public function __construct($config)
     {
-        $connectionString = $config['driver'] . ":" . $config['database'];
-        $this->db = new PDO($connectionString);
+        $this->db = new PDO($this->getDsn($config));
+    }
+
+    private function getDsn($config)
+    {
+        $driver = $config['driver'];
+        unset($config['driver']);
+
+        $dsn = $driver . ':' . http_build_query($config, '', ';');
+
+        if ($driver == 'sqlite') {
+            $dsn = $driver . ":" . $config['database'];
+        }
+
+        return $dsn;
     }
 
     public function query($query, $class = null, $params = [])
