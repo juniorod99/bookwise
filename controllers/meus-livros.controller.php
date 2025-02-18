df5671716641f630c9476ca546f87414874b253c
Junior <juniorod99@gmail.com>
2025-02-18 11:13:23 -0300

    Listando livros

--- controllers/meus-livros.controller.php
+++ controllers/meus-livros.controller.php
@@ -1,3 +1,13 @@
 <?php
+if (!auth()) {
+    header('location: /');
+    exit();
+}
 
-view('meus-livros');
+$livros = $database->query(
+    "select * from livros where usuario_id = :id",
+    Livro::class,
+    ['id' => auth()->id]
+);
+
+view('meus-livros', compact('livros'));
